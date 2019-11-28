9360366e01f08fb6051a48ae5780ea0c02657ecc
zhija <55587419+zhija@users.noreply.github.com>
2019-11-28 18:34:59 +0800

    Add files via upload

---
+++ P05/source/P05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a = 7;
+	int* aPtr = &a;
+	printf("The address of a is %p\nThe value of aPtr is %p", &a, aPtr);
+	printf("\n\nThe value of a is %d\nThe value of *aPtr is %d", a, *aPtr);
+	printf("\n\nShowing that * and & are complements of each other\n&*aPtr = %p\n*&aPtr = %p ", &*aPtr, *&aPtr);
+	system("pause");
+	return 0;
+}
